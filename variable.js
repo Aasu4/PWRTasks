845b83b6f851b4c7502ae027aa55ee82536794d7
Aasu4 <105736833+Aasu4@users.noreply.github.com>
2025-08-16 15:03:14 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+var  name = "Aiswarya";
+console.log(name);
+
+var number = 123;
+console.log(number);
+
+var isTrue = true;
+console.log(typeof isTrue);
+
+var mobilenumber = 545546565656566456n;
+console.log(mobilenumber);
+
+
+var a = 10;
+var b = 2;
+var c = a + b;
+console.log(c);
